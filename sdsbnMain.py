bedcc2444d7aa2d93bbf066f7995ea280c7876ef
zakwanmhmd <zakwanmahmud93@gmail.com>
2023-10-31 01:09:42 +0800

    Added some codes to make dictionary

--- sdsbnMain.py
+++ sdsbnMain.py
@@ -2,12 +2,23 @@
 import os
 import numpy as np
 from collections import Counter
+import pandas as pd
 
 #create word dictionary
 def make_dictionary(train_dir):
-    emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
-    allWords = []
-
+    words = pd.read_csv(train_dir).to_string()
+    dictionary = Counter(words.split())
+    
+    list_to_remove = dictionary.keys()
+    for item in list(list_to_remove):
+        if item.isalpha() == False:
+            del dictionary[item]
+        elif len(item) == 1:
+            del dictionary[item]
+    dictionary = dictionary.most_common(3000)
+    print(dictionary)
+    return dictionary
+    # print(emails[0])
     # for mail in emails:
     #     with open(mail) as m:
     #         for i, line in enumerate(m):
