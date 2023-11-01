2b9287153ca009d02805cd479d05752cbc88b23d
zakwanmhmd <zakwanmahmud93@gmail.com>
2023-11-01 23:20:21 +0800

    Finished make_dict func

--- sdsbnMain.py
+++ sdsbnMain.py
@@ -3,11 +3,25 @@ import os
 import numpy as np
 from collections import Counter
 import pandas as pd
+from nltk.corpus import stopwords      # contains list of stopwords
 
 #create word dictionary
 def make_dictionary(train_dir):
-    words = pd.read_csv(train_dir).to_string()
-    dictionary = Counter(words.split())
+    # ref https://www.kdnuggets.com/2020/07/spam-filter-python-naive-bayes-scratch.html
+    
+    train_set = pd.read_csv(train_dir)
+    train_set['email'] = train_set['email'].str.split()
+    vocab = []
+    for mail in train_set['email']:
+        try:
+            for word in mail:
+                vocab.append(str(word))
+        except TypeError:   # Data Set used has multiple errors when ran, so this is to catch it
+            #print("cant")
+            continue
+    
+    dictionary = Counter(vocab)
+    #print(len(vocab))       #print number of unique words in train set
     
     list_to_remove = dictionary.keys()
     for item in list(list_to_remove):
@@ -15,14 +29,13 @@ def make_dictionary(train_dir):
             del dictionary[item]
         elif len(item) == 1:
             del dictionary[item]
+        elif item in stopwords.words('english'):    # removes stop words
+            del dictionary[item]
+            
     dictionary = dictionary.most_common(3000)
-    print(dictionary)
+    #print(dictionary)
     return dictionary
-    # print(emails[0])
-    # for mail in emails:
-    #     with open(mail) as m:
-    #         for i, line in enumerate(m):
-    #             if i == 2
+    
 #feature extraction
 
 #datasets
@@ -31,4 +44,4 @@ train_dir = "spam_or_not_spam.csv"
 #train classifiers
 
 #print results
-make_dictionary(train_dir)
+print(make_dictionary(train_dir))
