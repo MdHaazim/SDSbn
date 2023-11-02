2107cc1bfe0cecde328f27273c290738c85d4914
Qamaruddeen <141305965+Qamaruddeen@users.noreply.github.com>
2023-11-02 15:27:05 +0800

    Update sdsbnMain.py

--- sdsbnMain.py
+++ sdsbnMain.py
@@ -82,8 +82,8 @@ train_dir = "spam_or_not_spam.csv"
 
 model1=MultinomialNB()
 model2=LinearSVC()
-model1.fit(feature_matrix,feature_labels)
-model2.fit(feature_matrix,feature_labels)
+model1.fit(feature_matrix,feature_names)
+model2.fit(feature_matrix,feature_names)
 
 #Test the unseen mails for Spam
 #test_dir= 'test-mails'
